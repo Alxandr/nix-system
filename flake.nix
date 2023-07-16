c2f3803c25e755ff4a57950dbf693b8ef07cc8fe
Aleksander Heintz <alxandr@alxandr.me>
2023-07-16 19:44:00 +0200

    Try different

--- flake.nix
+++ flake.nix
@@ -28,7 +28,9 @@
 		# There are many ways to reference flake inputs. The most widely used is github:owner/name/reference,
     # which represents the GitHub repository URL + branch/commit-id/tag.
 
-    # Official NixOS package source, using nixos's stable branch by default
+    # Pin our primary nixpkgs repository. This is the main nixpkgs repository
+    # we'll use for our configurations. Be very careful changing this because
+    # it'll impact your entire system.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
@@ -46,87 +48,29 @@
     # community wayland nixpkgs
     nixpkgs-wayland.url = "github:nix-community/nixpkgs-wayland";
 
-		# generate iso/qcow2/docker/... image from nixos configuration
-    nixos-generators = {
-      url = "github:nix-community/nixos-generators";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+		# # generate iso/qcow2/docker/... image from nixos configuration
+    # nixos-generators = {
+    #   url = "github:nix-community/nixos-generators";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
 
-		# secrets management, lock with git commit at 2023/7/15
-    agenix.url = "github:ryantm/agenix/0d8c5325fc81daf00532e3e26c6752f7bcde1143";
+		# # secrets management, lock with git commit at 2023/7/15
+    # agenix.url = "github:ryantm/agenix/0d8c5325fc81daf00532e3e26c6752f7bcde1143";
 
-		# my private secrets, it's a private repository, you need to replace it with your own.
-    # use ssh protocol to authenticate via ssh-agent/ssh-key, and shallow clone to save time
+		# # my private secrets, it's a private repository, you need to replace it with your own.
+    # # use ssh protocol to authenticate via ssh-agent/ssh-key, and shallow clone to save time
     # mysecrets = { url = "git+ssh://git@github.com/ryan4yin/nix-secrets.git?shallow=1"; flake = false; };
 	};
 
-	# The `outputs` function will return all the build results of the flake.
-  # A flake can have many use cases and different types of outputs,
-  # parameters in `outputs` are defined in `inputs` and can be referenced by their names.
-  # However, `self` is an exception, this special parameter points to the `outputs` itself (self-reference)
-  # The `@` syntax here is used to alias the attribute set of the inputs's parameter, making it convenient to use inside the function.
-	outputs =
-		inputs@{ self
-    , nixpkgs
-    , home-manager
-    , nixos-generators
-    , ...
-    }:
-
+	outputs = { self, nixpkgs, home-manager, ... }@inputs:
 		let
-      x64_system = "x86_64-linux";
-      x64_specialArgs = {
-        # use unstable branch for some packages to get the latest updates
-        pkgs-unstable = import inputs.nixpkgs-unstable {
-          system = x64_system; # refer the `system` parameter form outer scope recursively
-          # To use chrome, we need to allow the installation of non-free software
-          config.allowUnfree = true;
-        };
-      } // inputs;
+			mkVM = import ./lib/mkvm.nix;
 
-			# VirtualBox nixos test VM
-			host_test_modules = [
-				./hosts/nixos-test-vm
-
-				home-manager.nixosModules.home-manager
-        {
-          home-manager.useGlobalPkgs = true;
-          home-manager.useUserPackages = true;
-
-          home-manager.extraSpecialArgs = x64_specialArgs;
-          # home-manager.users.alxandr = import ./home/linux/x11.nix;
-          home-manager.users.alxandr = import ./home/linux/wayland.nix;
-        }
-			];
 		in {
-			nixosConfigurations = let system = x64_system; specialArgs = x64_specialArgs; in {
-				nixos-test-vm = nixpkgs.lib.nixosSystem {
-          inherit system specialArgs;
-          modules = host_test_modules;
-        };
+			nixosConfigurations.vm-test = mkVM "vm-test" rec {
+				inherit nixpkgs home-manager overlays;
+				system = "x86_64-linux";
+				users   = [ "alxandr" ];
 			};
-
-			formatter = {
-        # format the nix code in this flake
-        # alejandra is a nix formatter with a beautiful output
-        x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
-      };
-
-			# packages.x86_64-linux =
-			# 	# take images for hosts
-      #   #   https://github.com/nix-community/nixos-generators
-			# 	let system = x64_system; specialArgs = x64_specialArgs; in  {
-			# 		# VirtualBox nixos test VM
-			# 		nixos-test-vm = nixos-generators.nixosGenerate {
-			# 			inherit system specialArgs;
-			# 			modules = host_test_modules ++ [
-			# 				({config, ...}: {
-			# 					# proxmox.qemuConf.name = "aquamarine-nixos-${config.system.nixos.label}";
-			# 				})
-			# 			];
-
-			# 			format = "virtualbox";
-			# 		};
-			# 	};
 		};
 }
---
+++ lib/mkvm.nix
@@ -0,0 +1,41 @@
+# This function creates a NixOS system based on our VM setup for a
+# particular architecture.
+name: { nixpkgs, home-manager, system, users, overlays }:
+
+let
+	overlay_modules = [
+		# Apply our overlays. Overlays are keyed by system type so we have
+    # to go through and apply our system type. We do this first so
+    # the overlays are available globally.
+    { nixpkgs.overlays = overlays; }
+	];
+
+	machine_modules = [
+    ../hosts/${name}/default.nix
+	];
+
+	user_modules = map (user: ../users/${user}/nixos.nix) users;
+
+	home_manager_modules = map (user: home-manager.nixosModules.home-manager {
+		home-manager.useGlobalPkgs = true;
+		home-manager.useUserPackages = true;
+		home-manager.users.${user} = import ../users/${user}/home-manager.nix;
+	}) users;
+
+	modules = overlay_modules ++ machine_modules ++ user_modules ++ home_manager_modules;
+
+in
+	nixpkgs.lib.nixosSystem rec {
+		inherit system;
+
+		modules = modules ++ [
+			# We expose some extra arguments so that our modules can parameterize
+			# better based on these values.
+			{
+				config._module.args = {
+					currentSystemName = name;
+					currentSystem = system;
+				};
+			}
+		];
+	}
