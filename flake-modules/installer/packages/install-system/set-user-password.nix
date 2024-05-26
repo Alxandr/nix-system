5124f5564fa873e54026d39c70da4bc0bcdc7fbf
Aleksander Heintz <alxandr@alxandr.me>
2024-05-26 22:52:04 +0200

    use correct program

--- flake-modules/installer/packages/install-system/set-user-password.nix
+++ flake-modules/installer/packages/install-system/set-user-password.nix
@@ -1,6 +1,6 @@
 { writeShellApplication
 
-, gum, su }:
+, gum, shadow }:
 
 writeShellApplication {
   name = "set-user-password";
@@ -11,10 +11,10 @@ writeShellApplication {
 
 
     if ${gum}/bin/gum confirm "Allow login for $user?" ; then
-      ${su}/bin/passwd --root "$root" "$user"
+      ${shadow}/bin/passwd --root "$root" "$user"
       ${gum}/bin/gum format "✔️ User $user can login"
     else
-      ${su}/bin/passwd --root "$root" --delete "$user"
+      ${shadow}/bin/passwd --root "$root" --delete "$user"
       ${gum}/bin/gum format "❌ User $user cannot login"
       exit 1
     fi
