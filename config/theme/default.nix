2f1ca8619358c0e80816cbc04843f3b40f898014
Aleksander Heintz <alxandr@alxandr.me>
2025-01-24 00:36:23 +0100

    docs

--- config/theme/default.nix
+++ config/theme/default.nix
@@ -11,6 +11,9 @@
     (
       { config, ... }:
       {
+        # Something overwrites the default gtk2 config location, which causes home-manager
+        # to fail activation. This forces the file location elsewhere such that there is no
+        # conflict.
         config.gtk.gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
       }
     )
