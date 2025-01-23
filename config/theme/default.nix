d8bf2662506780cedbfe8c11c1885c41781831a4
Aleksander Heintz <alxandr@alxandr.me>
2025-01-24 00:34:29 +0100

    fix gtk2 issue?

--- config/theme/default.nix
+++ config/theme/default.nix
@@ -6,4 +6,13 @@
     polarity = "dark";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/chalk.yaml";
   };
+
+  config.home-manager.sharedModules = [
+    (
+      { config, ... }:
+      {
+        config.gtk.gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+      }
+    )
+  ];
 }
