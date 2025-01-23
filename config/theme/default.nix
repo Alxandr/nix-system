0ef83a83833a627aab0bab3317b7c5799d4d85b0
Aleksander Heintz <alxandr@alxandr.me>
2025-01-24 00:15:23 +0100

    enable theme

--- config/theme/default.nix
+++ config/theme/default.nix
@@ -1,6 +1,7 @@
-{ pkgs, ... }:
+{ lib, pkgs, ... }:
 {
   config.stylix = {
+    enable = lib.mkDefault true;
     image = ./bg.jpg;
     polarity = "dark";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/nord.yaml";
