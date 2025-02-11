1243f27cbdf4046212709dd8e644f5ffbbb75b97
Aleksander Heintz <alxandr@alxandr.me>
2025-02-11 23:41:18 +0100

    fix?

--- config/theme/default.nix
+++ config/theme/default.nix
@@ -9,60 +9,67 @@ let
 
 in
 {
-  config.stylix = {
-    enable = lib.mkDefault true;
-    image = wallpaper;
-    polarity = "dark";
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/chalk.yaml";
+  config = {
+    stylix = {
+      enable = lib.mkDefault true;
+      image = wallpaper;
+      polarity = "dark";
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/chalk.yaml";
 
-    fonts.monospace = {
-      name = "Fira Code";
-      package = pkgs.fira-code;
+      fonts.monospace = {
+        name = "Fira Code";
+        package = pkgs.fira-code;
+      };
+
+      cursor = {
+        package = pkgs.bibata-cursors;
+        name = "Bibata-Modern-Ice";
+      };
     };
 
-    cursor = {
-      package = pkgs.bibata-cursors;
-      name = "Bibata-Modern-Ice";
+    environment.systemPackages = [
+      (pkgs.writeTextDir "share/sddm/themes/breeze/theme.conf.user" ''
+        [General]
+        background=${wallpaper}
+        type=image
+      '')
+    ];
+
+    qt = {
+      style = lib.mkForce "breeze";
     };
-  };
 
-  config.environment.systemPackages = [
-    (pkgs.writeTextDir "share/sddm/themes/breeze/theme.conf.user" ''
-      [General]
-      background=${wallpaper}
-      type=image
-    '')
-  ];
+    home-manager.sharedModules = [
+      (
+        { config, ... }:
+        {
+          # Something overwrites the default gtk2 config location, which causes home-manager
+          # to fail activation. This forces the file location elsewhere such that there is no
+          # conflict.
+          config = {
+            gtk.gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
+            stylix.targets.hyprland.enable = false;
 
-  config.home-manager.sharedModules = [
-    (
-      { config, ... }:
-      {
-        # Something overwrites the default gtk2 config location, which causes home-manager
-        # to fail activation. This forces the file location elsewhere such that there is no
-        # conflict.
-        config = {
-          gtk.gtk2.configLocation = "${config.xdg.configHome}/gtk-2.0/gtkrc";
-          stylix.targets.hyprland.enable = false;
-
-          # https://github.com/danth/stylix/issues/835
-          qt = {
-            enable = true;
-            platformTheme.package = with pkgs.kdePackages; [
-              plasma-integration
-              # I don't remember why I put this is here, maybe it fixes the theme of the system setttings
-              systemsettings
-            ];
-            style = {
-              package = pkgs.kdePackages.breeze;
-              name = lib.mkForce "Breeze";
+            # https://github.com/danth/stylix/issues/835
+            qt = {
+              enable = true;
+              platformTheme.package = with pkgs.kdePackages; [
+                plasma-integration
+                # I don't remember why I put this is here, maybe it fixes the theme of the system setttings
+                systemsettings
+                breeze
+              ];
+              style = {
+                package = pkgs.kdePackages.breeze;
+                name = lib.mkForce "breeze";
+              };
+            };
+            systemd.user.sessionVariables = {
+              QT_QPA_PLATFORMTHEME = lib.mkForce "kde";
             };
           };
-          systemd.user.sessionVariables = {
-            QT_QPA_PLATFORMTHEME = lib.mkForce "kde";
-          };
-        };
-      }
-    )
-  ];
+        }
+      )
+    ];
+  };
 }
