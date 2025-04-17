719edcb7a4357e7be14985a465e8b9ac60ff865d
Aleksander Heintz <alxandr@alxandr.me>
2025-04-17 12:35:50 +0200

    fix: hyprpolkitagent

--- config/users/alxandr/home/hyprpolkitagent.nix
+++ config/users/alxandr/home/hyprpolkitagent.nix
@@ -8,33 +8,17 @@ let
   cfg = config.services.hyprpolkitagent;
 in
 {
-  options.services.hyprpolkitagent = {
-    enable = lib.mkEnableOption "Hyprpolkitagent, Hyprland's polkit agent";
-
-    package = lib.mkPackageOption pkgs "hyprpolkitagent" { };
-  };
-
   config = lib.mkIf cfg.enable {
     home.packages = [ pkgs.hyprland-qt-support ];
     systemd.user.services.hyprpolkitagent = {
-      Install = {
-        WantedBy = [ config.wayland.systemd.target ];
-      };
-
       Unit = {
-        Description = "Hyprland Polkit Authentication Agent";
         ConditionEnvironment = "WAYLAND_DISPLAY";
-        After = [ config.wayland.systemd.target ];
-        PartOf = [ config.wayland.systemd.target ];
         # X-Restart-Triggers = lib.mkIf (cfg.settings != { })
         #   [ "${config.xdg.configFile."hypr/hypridle.conf".source}" ];
       };
 
       Service = {
-        ExecStart = "${cfg.package}/libexec/hyprpolkitagent";
         Slice = "session.slice";
-        TimeoutStopSec = "5sec";
-        Restart = "on-failure";
       };
     };
   };
