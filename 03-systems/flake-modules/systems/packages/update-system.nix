65a40bef699434a2a19d1b74d0a0b353a151f827
Aleksander Heintz <alxandr@alxandr.me>
2026-02-14 12:53:49 +0100

    Add --refresh option to nh command in update-system.nix

--- 03-systems/flake-modules/systems/packages/update-system.nix
+++ 03-systems/flake-modules/systems/packages/update-system.nix
@@ -10,6 +10,6 @@ writeShellApplication {
   text = ''
     cmd=''${1:-boot}
     # ${pkgs.nixos-rebuild}/bin/nixos-rebuild "$cmd" --flake "${flakeMeta.configSpecifier}" --refresh
-    ${pkgs.nh}/bin/nh os "$cmd" "${flakeMeta.path}" --hostname "${flakeMeta.configKey}" --ask
+    ${pkgs.nh}/bin/nh os "$cmd" "${flakeMeta.path}" --hostname "${flakeMeta.configKey}" --ask --refresh
   '';
 }
