814c2e7d774181ea562873a3856d90d6db2230da
Aleksander Heintz <alxandr@alxandr.me>
2026-04-07 14:08:39 +0200

    op env

--- test.sh
+++
@@ -1,9 +0,0 @@
-TEMPDIR=$(mktemp -d /tmp/sshkeytest.XXXXXX)
-trap "rm -rf $TEMPDIR" EXIT
-
-# Export the key in pkcs8 format from 1password
-op.exe read "op://Private/Nix-system SOPS age key/private key" > $TEMPDIR/key
-chmod 600 $TEMPDIR/key
-
-# Convert the key to openssh format
-ssh-keygen -p -m PEM -f $TEMPDIR/key -q -N ""
