7196ca2b3eace9e24ebc2f50435bc184da613ac3
Aleksander Heintz <alxandr@alxandr.me>
2026-07-31 09:18:43 +0200

    remove rtk

--- config/systems/wsl/dev-env.nix
+++ config/systems/wsl/dev-env.nix
@@ -143,7 +143,6 @@
         pkgs.pnpm # package manager for node
         pkgs.powershell # shell
         pkgs.prek # pre-commit hooks
-        pkgs.rtk # cli proxy for reducing LLM token consumption
         pkgs.terraform # infrastructure as code tool
         pkgs.tmux # terminal multiplexer
         pkgs.yq-go # jq - but for yaml
